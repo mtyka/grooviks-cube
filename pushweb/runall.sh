a097d711f5a0798d688c1bf8bd397eeea926a035
Leo Dirac <leo.dirac@gmail.com>
2010-09-30 18:25:22 -0700

    Getting it to run again.  Grr...

--- pushweb/runall.sh
+++ pushweb/runall.sh
@@ -1,9 +1,10 @@
  #!/bin/bash
 
  ./authapp.py &
-hookbox -d -r bakonv8 -p 2974 --cbhost=127.0.0.1 --cbport=8080 --cbpath= &
-sleep 1
+sleep 2
+hookbox -d -r bakonv8 -a lair2low -p 2974 --cbhost=127.0.0.1 --cbport=8080 --cbpath= &
+sleep 2
 cd ../pacsci
-./groovikhookbox.py
+./groovikhookbox.py &
 #./random_colors.py &
 open http://localhost:8080
