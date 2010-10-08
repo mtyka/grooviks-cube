c2a8fbcaebcab524b200e7995a01d54e6efde965
Leo Dirac <leo.dirac@gmail.com>
2010-10-07 20:15:01 -0700

    Removing obsolete djangocube directory.

--- djangocube/syncstate/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from syncstate.models import Squares
-
-admin.site.register(Squares)
-
--- djangocube/syncstate/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('syncstate.views',
-    (r'^$', 'home'),
-    (r'^poll$', 'poll'),
-    (r'^rotate$', 'rotate'),
-)
