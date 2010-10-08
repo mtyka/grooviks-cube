52509eb5f1ee4df6d03617e1cbbb4ebcec47bfde
Leo Dirac <leo.dirac@gmail.com>
2010-10-07 21:29:48 -0700

    Updating readmes.

--- pushweb/runall.sh
+++ pushweb/runall.sh
@@ -1,12 +1,12 @@
  #!/bin/bash
 
- ./authapp.py &
+cd ../onecube
+./manage.py runserver 0.0.0.0:8080
 sleep 2
-hookbox -d -r bakonv8 -a lair2low -p 2974 --cbhost=127.0.0.1 --cbport=8080 --cbpath= &
+hookbox -d -r bakonv8 -a lair2low -p 2974 --cbhost=127.0.0.1 --cbport=8080 &
 sleep 2
 cd ../pacsci
 ./groovikhookbox.py &
-#./random_colors.py &
 open http://localhost:8080
 
 sleep 2
