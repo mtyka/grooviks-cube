5f95bdf3ad4b112ec51b091b1091f596af912970
Matt M <mail.matt.mcdonald@gmail.com>
2011-09-13 21:53:37 -0700

    -switching audio to mp3 from wav -switching soundmanager2 audio from html5 to flash -hooked up gear sounds (still needs some dev tuning on timing) -refactored sound code some in preparation for moving it out of controller.js

--- webserver/templates/control.js
+++ webserver/templates/control.js
@@ -3,64 +3,102 @@
 // ####################################################################
 {% include "soundmanager2-jsmin.js" %}
 
-// soundManager.url = "static/soundmanager2/swf";
-soundManager.preferFlash = false;
-soundManager.useHTML5Audio = true;
+soundManager.url = 'static';
+//soundManager.preferFlash = false;
+soundManager.useHTML5Audio = false;
+soundManager.flashVersion = 9;
+soundManager.useFastPolling = true;
+soundManager.useHighPerformance = true;
 soundManager.onload = function(){
     soundManager.createSound({
         id: 'column1_1',
-        url: 'static/synth_8bit_low_001.wav',
+        url: '/static/synth_8bit_low_001.mp3',
         autoLoad: true
     });
 
     soundManager.createSound({
         id: 'column1_2',
-        url: 'static/synth_8bit_med_001.wav',
+        url: '/static/synth_8bit_med_001.mp3',
         autoLoad: true
     });
 
     soundManager.createSound({
         id: 'column1_3',
-        url: 'static/synth_8bit_high_001.wav',
+        url: '/static/synth_8bit_high_001.mp3',
         autoLoad: true 
     }); 
 
      soundManager.createSound({
         id: 'column2_1',
-        url: 'static/synth_tal_low_002.wav',
+        url: '/static/synth_tal_low_002.mp3',
         autoLoad: true
     });
 
     soundManager.createSound({
         id: 'column2_2',
-        url: 'static/synth_tal_med_002.wav',
+        url: '/static/synth_tal_med_002.mp3',
         autoLoad: true
     });
 
     soundManager.createSound({
         id: 'column2_3',
-        url: 'static/synth_tal_high_002.wav',
+        url: '/static/synth_tal_high_002.mp3',
         autoLoad: true
     });    
     
     soundManager.createSound({
         id: 'column3_1',
-        url: 'static/vocoder_low_001.wav',
+        url: '/static/vocoder_low_001.mp3',
         autoLoad: true
     });
 
     soundManager.createSound({
         id: 'column3_2',
-        url: 'static/vocoder_med_001.wav',
+        url: '/static/vocoder_med_001.mp3',
         autoLoad: true
     });
 
     soundManager.createSound({
         id: 'column3_3',
-        url: 'static/vocoder_high_001.wav',
+        url: '/static/vocoder_high_001.mp3',
+        autoLoad: true
+    });
+    soundManager.createSound({
+        id: 'gear1',
+        url: '/static/gear_001.mp3',
+        autoLoad: true
+    });
+
+    soundManager.createSound({
+        id: 'gear2',
+        url: '/static/gear_002.mp3',
+        autoLoad: true
+    });
+
+    soundManager.createSound({
+        id: 'gear3',
+        url: '/static/gear_003.mp3',
         autoLoad: true
     });
 };
+
+function playFaceClickSound(frame){
+	var playerNumber = Math.floor(frame.payload[1]/ 3) + 1;
+	var columnNumber = (frame.payload[1] % 3) + 1;
+        var faceSound = soundManager.getSoundById('column'+ playerNumber  + '_' + columnNumber);
+	soundManager.stopAll();
+
+	var gear1 = soundManager.getSoundById('gear1');
+	var gear2 = soundManager.getSoundById('gear2');
+	var gear3 = soundManager.getSoundById('gear3');
+
+	faceSound.play({onfinish:function(){ 
+	gear1.play({onfinish: function(){
+		gear2.play({onfinish: function(){
+			gear3.play();
+		}});
+	 }})}});
+}
 // ####################################################################
 // ######################## Control Logic #############################
 // ####################################################################
@@ -257,11 +295,7 @@ function establish_hookbox_connections() {
         if( channelName == 'faceclick' ) {
             faceclick_subscription = _subscription;                
             faceclick_subscription.onPublish = function(frame) {
-		var playerNumber = Math.floor(frame.payload[1]/ 3) + 1;
-		var columnNumber = (frame.payload[1] % 3) + 1;
-                var faceSound = soundManager.load('column'+ playerNumber  + '_' + columnNumber);
-		soundManager.stopAll();
-		faceSound.play(); 
+		playFaceClickSound(frame);
                 clog('Heard about click on face ' + frame.payload);
             };  
         }
