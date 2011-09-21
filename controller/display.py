51ee542c826cfc548e76b0237608eae15a7dbcf9
ChimeraScorn <ChimeraScorn@localhost>
2011-09-20 18:03:51 -0700

    Add logic to prevent attempting to USBs if we have 11 active arduinos

--- controller/display.py
+++ controller/display.py
@@ -56,47 +56,47 @@ class Display:
          self.lastLogged = now;
          log = False;
          logLines = [];
+
       #print "looping";
       #  Here we need to:
       #    Handle light arduinos
       for i in range(self.cTrackers):
-         clock1 = time.clock() #remove me
          tracker = self.trackers[i];
          #         Decode all waiting pong messages
          tracker.read();      
-         clock2 = time.clock()
          if (tracker.lastgood != None and log):
             logLines.append("ID: {0:c}, PW1: {1:d}, PW2: {2:d}\n".format((tracker.lastgood.guid + ord('a') - 1), ord(tracker.lastgood.powermodule1), ord(tracker.lastgood.powermodule2)));
+
          #         Make sure "last" state matches our expected state
          # if we haven't heard from one in a "long time", then set it to faulted.
          if (tracker.timesince() > 60 and tracker.timesincetried() > 30):
             # if we had been tracking this, and it had been mapped, and this is the first time we've failed to track it,
             # unmap it, note that we've done so.
             if (tracker.lastgood != None):
+               print "Dropping board %d because of non-responsiveness" % tracker.guid;
                self.lm.dropBoard(tracker);
                tracker.lastgood = None;
+
+            # only try to reconnect if we are not actively tracking all arduinos
+            if (self.lm.countTrackers < 11):
+               print "Reconnecting to lightboard ";
+               print i;
+               #tracker.close();
+               if (platform.system() == 'Darwin' ):
+                   tracker.openSerial('/dev/tty.usbserial-A6008iGf');
+               else:
+                   tracker.openSerial(i);
+               tracker.triednow();
             
-            print "Reconnecting to lightboard ";
-            print i;
-            #tracker.close();
-            if (platform.system() == 'Darwin' ):
-                tracker.openSerial('/dev/tty.usbserial-A6008iGf');
-            else:
-                tracker.openSerial(i);
-            tracker.triednow();
-         clock3 = time.clock()
          if (tracker.lastgood != None and tracker.guid != tracker.lastgood.guid):
             #drop the old board from the mapping
             if (tracker.guid != 0):
                self.lm.dropBoard(tracker);
             #update the guid on the tracker
             tracker.guid = tracker.lastgood.guid;
-         clock4 = time.clock()
          if (tracker.lastgood != None and not self.lm.tracking(tracker)):
             #update our mapping as appropriate
             self.lm.addBoard(tracker);
-         clock5 = time.clock()
-         #print "Disp : " + str( clock2 - clock1 ) + " -- " + str( clock3 - clock2 ) + " -- " + str( clock4 - clock3 ) + " -- " + str( clock5 - clock4 )
       if log:
          self.logger.logLines(logLines);
 
