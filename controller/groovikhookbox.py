ced9fe738c9f3596de0fc99f8ca2c265b86d737b
ChimeraScorn <ChimeraScorn@localhost>
2011-09-27 23:36:05 -0700

    This change makes the display driver search through the entire 128 port space. As we have intermittent connectivity issues, each time a board is lost and reacquired, the OS is assigning the next tty port. After a number of dropped connections which successfully reacquire (14... The 15th will fail), the board gets a tty port out of our defined search space. It should be safe to cap the search space at 128, as that's the limit of how many USB devices can be connected. We should test that at some point to verify that the OS doesn't just continue assigning higher numbers after that, but I'd be surprised.

--- controller/groovikhookbox.py
+++ controller/groovikhookbox.py
@@ -186,7 +186,7 @@ class Cube():
     def __init__(self):
         self.lastRotationStep = 0
 
-        count = 25
+        count = 129;
         self.displayc = display.Display(count, "input_playa.py" )
         
         # connect to the hookbox client and receive commands
