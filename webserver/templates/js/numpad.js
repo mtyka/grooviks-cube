c0a381f4d25e86ccb5288b52a8e235c9ee3b934d
Mike Tyka <mike.tyka@gmail.com>
2013-08-12 14:01:07 -0700

    Added a number pad and an escape mechanism to get out of game mode back to the admin page or reload the station with a simple code lock. Now - the code is just in plain view in the javascript - the point here is *NOT* to foil anyone with full access to the machine - they could just type in the admin URL anyway. The point is to prevent the museum visitor that has no access to the browser itself to escape the kiosk mode. A slightly more secure way would be to the have the server do the code check rather than the client.

---
+++ webserver/templates/js/numpad.js
@@ -0,0 +1,65 @@
+
+var Numpad = (function(){
+  var my = {}
+
+  my.number_write = function(x)
+  {
+    var text_box = document.getElementById("number");
+    if(x>=0 && x<=9)
+    {
+      if(isNaN(text_box.value))
+     text_box.value = 0;
+   text_box.value = (text_box.value * 10)+x;
+    }
+  }
+  my.number_clear = function()
+  {
+    document.getElementById("number").value = 0;
+  }
+  
+  my.number_enter = function( action )
+  {
+    var text_box = document.getElementById("number");
+    var num = text_box.value;
+
+    // just to be clear - this is NOT supposed to be a secure lock or anything - 
+    // only to prevent anyone without keyboard access from escaping from this page 
+    if( num == '761862' ){
+      if ( action == "admin" ){
+        window.location.assign( "http://" +  window.location.host + "/admin_cube" ) 
+      } else 
+      if ( action == "reload" ){
+        window.location.reload(true);
+      }
+    }else{
+      alert("Code incorrect")
+    }
+  }
+
+  my.show = function(){
+    $("#admin_background").fadeIn()
+  }
+  
+  my.hide = function(){
+    $("#admin_background").fadeOut()
+  }
+
+$( function(){ 
+  $("#footer").click(function(){
+    Numpad.number_clear()
+    Numpad.show()
+    });
+
+  $("#admin_background").click(function(){
+    Numpad.number_clear()
+    Numpad.hide()
+    });
+
+  $("#securitycode").click(function(){
+    return false; 
+    });
+})
+
+  return my;
+}())
+  
