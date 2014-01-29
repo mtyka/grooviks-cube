b2101b4da66cb271fa81a48c8e8532ee615f9651
Tristan W. <tristan@ideum.com>
2014-01-29 16:35:20 -0700

    this enables voting, forgot to add it

---
+++ controller/voteTracker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+class voteTracker:
+
+	def __init__(self, population, position):
+		self.voteOpen = False
+		self.population = population
+		self.candidate = position
+		self.voted = []
+		self.__voteSum = 0
+
+	def startVote(self):
+		self.voteOpen = True
+
+	def closeVote(self):
+		self.voteOpen = False
+
+	def handleVote(self, position, vote):
+		if type(vote) is not int:
+			print "something wrong with the vote."
+			return
+		elif not self.voteOpen:
+			print "not open for votes"
+			return
+
+		self.voted.append(position)
+		self.__voteSum += vote
+
+	def voteStatus(self):
+		return self.__generateVoteStatus()
+
+	def __generateVoteStatus(self):
+		if not self.voteOpen:
+			return "closed"
+		elif len(self.voted) < self.population:
+			return "waiting"
+		elif len(self.voted) == self.population and self.__voteSum == self.population:
+			return "success"
+		else:
+			return "failure"
